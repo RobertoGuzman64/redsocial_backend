5a99080b89f3a52c3a8f56aac617ba03afad761d
RobertoGuzman <robertoguzman64@gmail.com>
2022-03-24 19:54:17 +0100

    fix: Añadida el router a index.js para que la api tenga acceso a las rutas

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const cors = require('cors');
 const dbconnect = require('./db/dbconnect');
 const PORT = process.env.PORT || 5500;
+const router = require('./router')
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 dbconnect();
 
