7ba311b16eae6c92714499b01a9ff610609382e3
RobertoGuzman <robertoguzman64@gmail.com>
2022-03-24 19:13:08 +0100

    feat:Añadidos los endpoints de UsuarioRouter POST'/' crearUsuario y POST'/login' loginUsuario

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,29 +1,44 @@
 const UsuarioModel = require('../models/usuario.js');
 const bcrypt = require("bcrypt");
+const authConfig = require('../config/auth.js');
+
+// Clase Usuario donde contiene todas las funciones.
 
 class Usuario{
     constructor(){
     }
+    async crearUsuario(body) {
+        body.clave = await bcrypt.hash( body.clave, authConfig.rondas );
+        return UsuarioModel.create(body);
+    }
     async traerUsuarios(){
         return UsuarioModel.find();
     }
-    
+    async loginUsuario(body) {
+        let correo = body.correo;
+        let clave = body.clave;
+        let token = jwt.sign;
+        UsuarioModel.findOne({where : {correo : correo}}).then(usuarioEncontrado => {
+            if (!usuarioEncontrado){
+                return "Usuario o contraseña inválido"
+            }else {
+                //el usuario existe, por lo tanto, vamos a comprobar
+                //si el password es correcto
+                if (bcrypt.compareSync(clave, UsuarioModel.clave)) {
+                    let token = jwt.sign({ usuario: UsuarioModel }, authConfig.complemento, {
+                        expiresIn: authConfig.expiracion
+                    });
+                    return{
+                        usuario: UsuarioModel,
+                        token: token
+                    }
+                } else {
+                    return { msg: "Usuario o contraseña inválidos" };
+                }
+            };
+        });
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 let UsuarioController = new Usuario();
--- views/UsuarioRouter.js
+++ views/UsuarioRouter.js
@@ -3,7 +3,9 @@ const router = require("express").Router();
 const UsuarioController = require('../controllers/UsuarioController');
 
 
-//GET - Encuentra todos los datos de todos los Usuarios.
+// GET 
+// Endpoint de mostrar Todos los Usuarios.
+// http://localhost:5500/usuarios
 
 router.get("/", async(req, res) => {
     try {
@@ -15,8 +17,38 @@ router.get("/", async(req, res) => {
     }
 });
 
+// POST
+// Endpoint de crear un Usuario.
+// http://localhost:5500/usuarios
+
+router.post("/", async(req, res) => {
+    try {
+        const body = req.body;
+        res.json(await UsuarioController.crearUsuario(body));
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+// POST
+// Endpoint de Login de Usuario.
+// http://localhost:5500/usuario/login
+
+router.post("/login", async(req, res) => {
+    try {
+        const body = req.body;
+        res.status(200).json(await UsuarioController.loginUsuario(body));
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
 
 
+// Endpoint de Modificar el perfil por ID.
 
 
 
