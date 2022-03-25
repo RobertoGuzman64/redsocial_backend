c398fc8d6b6902004c6208979b9fd6bded3f36c8
Luigi Minardi <lugminsinlag@gmail.com>
2022-03-25 07:19:29 +0100

    build: Añadiendo jest y supertest al proyecto para empezar con el testeo de la aplicacion. Tambien abstraendo el app del index.js a server.js para poder hacer el testing en otro puerto.

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
 const dbconnect = require('./db/dbconnect');
 const PORT = process.env.PORT || 5500;
-const router = require('./router')
-
-app.use(express.json());
-app.use(cors());
-app.use(router);
+const app = require('./server');
 
 dbconnect();
 
